d3e7a51805586cb85e57c97788a5eac1781270ea
Adnan Md. Wahidur Rahman <onlyshishir@gmail.com>
2019-11-23 23:48:56 +0600

    Qfix long L

--- schools/templates/pages/discussions.py
+++ schools/templates/pages/discussions.py
@@ -7,8 +7,8 @@ from frappe.website.utils import get_comment_list
 
 def get_context(context):
 	context.doc = frappe.get_doc('Discussion', frappe.form_dict.discussion)
-	portal_items = [{'reference_doctype': u'Topic', 'route': u"/topic?course=" + str(context.doc.course), 'show_always': 0L, 'title': u'Topics'},
-				{'reference_doctype': u'Discussion', 'route': u"/discussion?course=" + str(context.doc.course), 'show_always': 0L, 'title': u'Discussions'},
+	portal_items = [{'reference_doctype': u'Topic', 'route': u"/topic?course=" + str(context.doc.course), 'show_always': 0, 'title': u'Topics'},
+				{'reference_doctype': u'Discussion', 'route': u"/discussion?course=" + str(context.doc.course), 'show_always': 0, 'title': u'Discussions'},
 
 	]
 	context.show_sidebar = True
